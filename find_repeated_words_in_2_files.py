fbb4760000a1a29cee22cd234a91ea3002f3d3c3
ChunyaSong <83945386+ChunyaSong@users.noreply.github.com>
2025-03-24 17:02:14 +0100

    find_repeated_words_in_2_files.py

--- find_repeated_words_in_2_files.py
+++ find_repeated_words_in_2_files.py
@@ -1,14 +1,15 @@
 import os
 import pandas as pd
 
-path_lex = "G:/reading_battery/reading_battery_sti/lexical"
+# replace the paths in your need
+path_lex = "G:/reading_battery/reading_battery_sti/lexical" 
 path_sem = "G:/reading_battery/reading_battery_sti/semantic"
 path_og_words = "G:/reading_battery/reading_battery_sti/words_4_antpost"
 
-# 确保输出文件夹存在（不存在则自动创建）
+# make sure the output path exists, or create one
 os.makedirs(path_lex, exist_ok=True)
 
-# 构建文件路径
+# paths of files that will be input and output
 input_file_lex = os.path.join(path_lex, 'lex_list.xlsx')
 input_file_sem = os.path.join(path_sem, 'sem_list.xlsx')
 input_file_og = os.path.join(path_og_words, 'words_450.xlsx')
@@ -17,46 +18,33 @@ output_file_lex_unique = os.path.join(path_lex, 'non_duplicated.csv')
 output_file_sem_common = os.path.join(path_sem, 'repeated_words.csv')
 output_file_sem_unique = os.path.join(path_sem, 'non_duplicated.csv')
 
-# 读取两个 xlsx|csv 文件, header=None确保第一行被视为数据而不是index,index_col=None确保第一列被视为数据而不是index
-# df_A = pd.read_excel(input_file_lex, header=None)
-# df = pd.read_csv('file.csv', encoding='utf-8')  # 原来的文件是csv文件的话，可以指定编码是utf-8,让其比对特殊字符时不会报错
-
+# read the files
+# compare lex_list.xlsx VS words_450.xlsx
 df_A = pd.read_excel(input_file_lex, header=None)
-#df_A = pd.read_excel(input_file_sem, header=None)
 df_B = pd.read_excel(input_file_og, header=None)
+# uncomment line 27-28 to compare sem_list.xlsx VS words_450.xlsx, but do not forget to comment line 23-24
+# df_A = pd.read_excel(input_file_sem, header=None)
+# df_B = pd.read_excel(input_file_og, header=None)
 
-
-# df_A = pd.read_excel('G:/reading_battery/reading_battery_sti/lexical/lex_list.xlsx.xlsx', header=None, index_col=None)
-# df_B = pd.read_excel('words_450.xlsx', header=None, index_col=None)
-# df_A = pd.read_csv('rl_words_450.csv', header=None, index_col=None)
-# df_B = pd.read_csv('words_450.csv', header=None, index_col=None)
-
-
-
-# 提取 A 文件中的文件名，并去掉 .bmp 后缀
-# words_A = df_A.iloc[:, 0].str.replace('.bmp', '', regex=False)
+# get words in df_A and df_B
 words_A = df_A.iloc[:, 0]
-
-# 提取 B 文件中的单词
 words_B = df_B.iloc[:, 0]
 
-# 找出重复项（交集）
+# find the repeated words
 common = words_A[words_A.isin(words_B)].drop_duplicates()
 non_duplicates = words_A[~words_A.isin(words_B)].drop_duplicates()
 
-
-# 显示或保存结果
+# print the results: how many words are repeated or non-duplicated
 print(f'Found {len(common)} repeated items：')
 print(common)
 print(f'Found {len(non_duplicates)} non-duplicated items：')
 print(non_duplicates)
 
+# save results to corresponding path
+# save results to lexical folder
+common.to_frame(name='word').to_csv(output_file_lex_common, index=False)
+non_duplicates.to_frame(name='word').to_csv(output_file_lex_unique, index=False)
 
-# 保存结果到输出目录
-# 保存到lex文件夹下
-# common.to_frame(name='word').to_csv(output_file_lex_common, index=False)
-# non_duplicates.to_frame(name='word').to_csv(output_file_lex_unique, index=False)
-
-# 保存到sem文件夹下
-common.to_frame(name='word').to_csv(output_file_sem_common, index=False)
-non_duplicates.to_frame(name='word').to_csv(output_file_sem_unique, index=False)
+# uncomment line 49-50 to save results to semantic folder
+# common.to_frame(name='word').to_csv(output_file_sem_common, index=False)
+# non_duplicates.to_frame(name='word').to_csv(output_file_sem_unique, index=False)
